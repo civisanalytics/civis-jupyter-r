37d45dd8fb46950d64f20c9c3481f5fc47e042a7
Patrick Miller <pmiller@civisanalytics.com>
2020-01-06 17:34:56 -0500

    dont install civis-r again

--- setup.R
+++ setup.R
@@ -1,7 +1,3 @@
-# Install civis R client
-options(unzip='internal');
-devtools::install_github('civisanalytics/civis-r', ref = 'v2.1.0', upgrade_dependencies = FALSE);
-
 # Install R Kernel for Jupyter
 install.packages(c('IRdisplay', 'pbdZMQ'))
 devtools::install_github('IRkernel/IRkernel', ref = '1.0.0', upgrade_dependencies = FALSE);
