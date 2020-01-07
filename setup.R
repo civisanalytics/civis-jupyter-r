08242682981314feb6468d54c7f6773710702800
Patrick Miller <pmiller@civisanalytics.com>
2020-01-07 10:39:33 -0500

    - datascience-r -> 3.2.0 - rocker/verse -> 3.6.1 - 30+ packages added to requirements.txt - Geospatial system dependencies- Many new packages - IRkernel -> 1.0.2

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 # Install R Kernel for Jupyter
 install.packages(c('IRdisplay', 'pbdZMQ'))
-devtools::install_github('IRkernel/IRkernel', ref = '1.0.0', upgrade_dependencies = FALSE);
+devtools::install_github('IRkernel/IRkernel', ref = '1.0.2');
 
 # kernel name = ir
 IRkernel::installspec()
