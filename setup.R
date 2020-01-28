41b5218fc483d282d833e5341bcfe22e6db6b13b
Patrick Miller <pmiller@civisanalytics.com>
2020-01-28 16:04:45 -0500

    python 3.7

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 # Install R Kernel for Jupyter
 install.packages(c('IRdisplay', 'pbdZMQ'))
-devtools::install_github('IRkernel/IRkernel', ref = '1.0.2')
+devtools::install_github('IRkernel/IRkernel', ref = '1.1')
 
 # kernel name = ir
 IRkernel::installspec()
